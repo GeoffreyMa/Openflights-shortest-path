b2c2f11df5f039347944825bcfac4c4e1005afbe
Kevin Lim <wzlim2@illinois.edu>
2020-12-01 11:47:56 -0600

    class airports

---
+++ airports.h
@@ -0,0 +1,20 @@
+/**
+ * @file Airports.h
+ */
+
+ #include <stdlib.h>
+ #include <iostream>
+ #include <fstream>
+ #include <sstream>
+ #include <vector>
+ #include <utility>
+
+ using namespace std;
+
+ class Airports {
+    public:
+        Airports(string txt);
+
+    private:
+        vector<pair<pair<double, double>, unsigned>> airportmap;
+ }

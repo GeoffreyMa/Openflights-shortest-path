f525b6a38622dfdd6755223b931f004ac8c3c4d9
Chiao Ting Huang <cthuang2@linux-a2.ews.illinois.edu>
2020-12-03 03:04:58 -0600

    fix airportmap file and add airport objects

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#include "airports.h"
+
+#include "airportsmap.h"
 #include <iostream>
 
 
 int main (int argc, const char * argv[]) {
+    std::string airport_file = argv[1];
+    AirportsMap a_map(airport_file);
     return 0;
 }
